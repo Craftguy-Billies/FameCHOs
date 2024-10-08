83cc82666181369b0b2bb742a53468a354077c5f
Craftguy-Billies <69667779+Craftguy-Billies@users.noreply.github.com>
2024-10-08 12:04:39 +0800

    Update autonews.py

--- autonews.py
+++ autonews.py
@@ -933,9 +933,6 @@ def write_file(file_path, content, title, source, model):
         </div>
 	'''
         file.write(then)
-        embed_code = get_first_youtube_embed(title, model)
-        if embed_code:
-            file.write(embed_code + '\n\n')
         # Split content into lines
         lines = content.splitlines()
 
@@ -951,7 +948,11 @@ def write_file(file_path, content, title, source, model):
                 if processed_line:  # Only write non-empty lines
                     file.write(processed_line)
                 
-        file.write('\n<p>資料來源： ' + source + '</p>\n</div>\n</main>\n\n')
+        file.write('\n<div class ="news-vid-outer">\n<div class="related-vid-text-outer title-bar ">\n')
+        embed_code = get_first_youtube_embed(title, model)
+        if embed_code:
+            file.write(embed_code + '\n\n')
+        file.write('<p class="related-vid-text">相關影片：</p>\n</div>\n\n<p class="news-text-inner">資料來源： ' + source + '</p>\n</div>\n</div>\n</main>\n\n')
 
         footer = r'''
  <footer class="footer">
