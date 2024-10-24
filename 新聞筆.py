d86018953e18737511d30cd1e71b4be2e2fa01b7
Nathanpoonst0116 <107419638+Nathanpoonst0116@users.noreply.github.com>
2024-10-24 15:27:31 +0800

    Update 新聞筆.py

--- 新聞筆.py
+++ 新聞筆.py
@@ -1203,7 +1203,7 @@ rss_urls = [
     ['https://www.koreaherald.com/common/rss_xml.php?ct=105', 'Korea Herald', 'K-Pop'],
     ['https://tokyocheapo.com/feed/', 'Tokyo Cheapo', 'Others'],
     ['https://www.koreatimes.co.kr/www/rss/entertainment.xml', 'TheKoreaTimes', 'K-Pop'],
-    ['https://en.yna.co.kr/RSS/culture.xml', 'Yonhap News Agency', 'K-Pop'],
+    ['https://en.yna.co.kr/RSS/culture.xml', 'Yonhap News Agency', 'Drama'],
     ['https://j-generation.com/feed/', 'J-GENERATION', 'J-Pop'],
     ['https://phoenixtalkspopculturejapan.wordpress.com/2024/09/08/the-half-way-mark-with-bakuage-sentai-boomboonger/', 'Phoenix Talks Pop Culture Japan', 'Drama']
 ]
